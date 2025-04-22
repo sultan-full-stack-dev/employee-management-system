52d1e5c79f4e22aae6a6fbce6b25eb8063e3bd43
FYIT80SULTAN <sultanshaikh1029@gmail.com>
2025-04-22 15:50:19 +0530

    Test Push from dev branch

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+class Controller
 {
-    //
+    public function test(){
+        return view('test');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test',[Controller::class,'test']);
